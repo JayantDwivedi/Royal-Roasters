59bdfbab5819628cf98c266fe9f51815398bc369
JayantDwivedi <jayantdwivedi19@gmail.com>
2020-09-18 22:14:34 +0530

    added about page

---
+++ about.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Royal Roasters - About us</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/about.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+
+<body>
+
+<div class="row header-sec">
+        <div class="col-lg-2 col-md-6">
+            <div class="logo">
+                <img src="img/logo.PNG" class="img-logo" alt="logo">
+            </div>
+        </div>
+        <div class="col-lg-10 col-md-6">
+            <div class="brand comp-name">Royal Roasters</div>
+            <div class="address-bar">Balaji Puram | Ashram Road | Mainpuri 205001</div>
+        </div>
+    </div>
+
+    <!-- Navigation -->
+    <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>Royal Roasters</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <style>
+                        .embed-container {
+                            position: relative;
+                            padding-bottom: 56.25%;
+                            height: 0;
+                            overflow: hidden;
+                            max-width: 100%;
+                        }
+
+                        .embed-container iframe,
+                        .embed-container object,
+                        .embed-container embed {
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                        }
+                    </style>
+                    <div class='embed-container'><iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="788.54" height="443" type="text/html" src="https://www.youtube.com/embed/q9o16fVt4PQ?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"><div><small><a href="https://youtubeembedcode.com/nl/">youtubeembedcode nl</a></small></div><div><small><a href="https://www.smartmenus.org/">buy fast web traffic</a></small></div></iframe></div>
+                </div>
+                <div class="col-md-6">
+                    <p>The Royal Roasters launched is launched in 2020, in the heart of Mainpuri. Our mission is simple. We want to teach coffee drinkers all over the world how to make the perfect blend!
+                        Our Coffee experts have a lifelong passion for coffee. We provide a coffee some lines.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Our
+                        <strong>Team</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive img-team" src="img/jayant.jpeg" alt="">
+                    <h3>Jayant</h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive img-team" src="img/abt2.jpg" alt="">
+                    <h3>Jane Brown</h3>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive img-team" src="img/abt3.jpg" alt="">
+                    <h3>Anne Marie</h3>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                <p>Made with &#10084; by Jayant</p>
+                <div class="icons">
+                        <ul>
+                            <li><i class="fa fa-facebook"></i></li>
+                            <li><i class="fa fa-twitter"></i></li>
+                            <li><i class="fa fa-google-plus"></i></li>
+                            <li><i class="fa fa-linkedin"></i></li>
+                            <li><i class="fa fa-dribbble"></i></li>
+                        </ul>
+                    </div>
+                    <p>Copyright &copy; <em>Royal Roasters </em> 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
